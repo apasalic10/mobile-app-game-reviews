3c1581f1ce28c0b07281cae099e00459a101c170
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-03-28 22:06:10 +0200

    created home_activity.xml file

---
+++ app/src/main/java/com/example/rma23_19079_videogames/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.rma23_19079_videogames
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
