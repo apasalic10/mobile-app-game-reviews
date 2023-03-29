270d08ae1f87e0f5064f1b7b877694a41574018c
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2023-03-29 02:03:27 +0200

    updated search function

--- app/src/main/java/com/example/rma23_19079_videogames/MainActivity.kt
+++ app/src/main/java/com/example/rma23_19079_videogames/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.rma23_19079_videogames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var gameListView: RecyclerView
     private lateinit var gameListAdapter: GameListAdapter
     private var gameList =  getAll()
+    private lateinit var searchButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_activity)
@@ -26,6 +29,27 @@ class MainActivity : AppCompatActivity() {
         gameListView.adapter = gameListAdapter
         gameListAdapter.updateMovies(gameList)
 
+        val homeButton = findViewById<Button>(R.id.home_button)
+        homeButton.isEnabled = false
+
+        searchButton = findViewById(R.id.search_button)
+        searchButton.setOnClickListener {
+            val searchText = findViewById<EditText>(R.id.search_query_edittext)
+
+            if(searchText.text.toString().isEmpty()){
+                gameListAdapter.updateMovies(gameList)
+            }
+            else{
+                val searchGame = getDetails(searchText.text.toString())
+
+                if(searchGame === null){
+                    gameListAdapter.updateMovies(listOf())
+                }
+                else{
+                    gameListAdapter.updateMovies(listOf(searchGame))
+                }
+            }
+        }
 
     }
 }
